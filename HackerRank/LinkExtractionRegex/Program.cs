c096250262b3c0b00d50e33d1c7128e9e58307a2
Deniz YALCIN <deniz@raklet.com>
2016-07-31 00:00:09 +0200

    First commit - tested

--- HackerRank/LinkExtractionRegex/Program.cs
+++ HackerRank/LinkExtractionRegex/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LinkExtractionRegex
@@ -10,6 +11,78 @@ namespace LinkExtractionRegex
     {
         static void Main(string[] args)
         {
+            //string line = Console.ReadLine();
+
+            string line = "";
+            int lineCount = 0;
+
+            System.IO.StreamReader file = null;
+            try
+            {
+                file = new System.IO.StreamReader(@"..\..\test4.txt");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("File not found!");
+                throw new Exception();
+            }
+
+            if ((line = file.ReadLine()) == null)
+            {
+                Console.WriteLine("Invalid file");
+                return;
+            }
+
+            if (!int.TryParse(line, out lineCount) || lineCount < 1)
+            {
+                Console.WriteLine("Invalid first line");
+                return;
+            }
+
+            for (int i = 0; i < lineCount; i++)
+            {
+                line = file.ReadLine();
+
+                if (line == null)
+                {
+                    Console.WriteLine("Invalid line - line number : " + (i + 1));
+                    return;
+                }
+
+                //find a tag and get outer html
+                //Regex rex = new Regex("<\\ba\\b[^>]*>[^<]*(?:<(?!<\\/a>))*\\/\\ba\\b>");
+                Regex rex = new Regex("<\\s*a\\s*[^>]*href\\s*=\\s*(?:\"|')([^\"']*)(?:\"|')(!?.(?!<\\s*\\/\\s*a))*.<\\s*\\/\\s*a[^>]*>");
+
+                Match match = rex.Match(line);
+
+                while (match.Success)
+                {
+                    string matchString = match.Value;
+
+                    // a tag find href inner text
+                    Regex findHref = new Regex("\\s*a\\s*[^>]*href\\s*=\\s*(?:\"|')([^\"']*)(?:\"|')");
+
+                    // find tags in match
+                    Regex removeTags = new Regex("<[^>]*>");
+
+                    string href = findHref.Match(matchString).Success ? findHref.Match(matchString).Groups[1].Value : "";
+                    string contentText = removeTags.Replace(matchString, "");
+
+                    //Trim results
+                    href = string.IsNullOrEmpty(href) ? "" : href.Trim();
+                    contentText = string.IsNullOrEmpty(contentText) ? "" : contentText.Trim();
+
+                    Console.WriteLine(href + "," + contentText);
+
+                    //search same line
+                    line = line.Replace(matchString, "");
+                    match = rex.Match(line);
+                }
+            }
+            file.Close();
+
+            Console.ReadLine();
+            return;
         }
     }
 }
